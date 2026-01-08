fe3d95039188022c3c9e138dc8ad8b8c56c326b2
Hajar-bch <hajarbacha46@gmail.com>
2026-01-08 12:05:54 +0100

    fin question 6

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,14 +4,26 @@ public class Gaulois {
 	private String nom;
 	private int force;
 
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+
+	}
+
 	public String getNom() {
 		return nom;
 
 	}
-
-	public Gaulois(String nom, int force) {
-		this.nom = nom;
-		this.force = force;
+	
+	//main C
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		System.out.println(asterix.getNom());
+		asterix.parler("Bonjour à tous");
+	}
+	
+	public void parler(String texte) {
+		System.out.println("Le gaulois " + nom + ": «" + texte + "»");
 
 	}
 }
