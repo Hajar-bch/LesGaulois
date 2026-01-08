83359867bb0f13118be4ab970a48e3a01990aaa3
Hajar-bch <hajarbacha46@gmail.com>
2026-01-08 11:13:07 +0100

    fin des premieres questions

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,17 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+
+	public String getNom() {
+		return nom;
+
+	}
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+
+	}
+}
